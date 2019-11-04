69944337de41f234ef63fcf868daa23663003b91
KarolKubala <56908308+KarolKubala@users.noreply.github.com>
2019-11-04 22:34:44 +0100

    Update Zelki.py

--- Zelki.py
+++ Zelki.py
@@ -28,3 +28,17 @@ while True:
 	# bierzemy frame z wideostreamu i zmieniamy jego wymiar na max 400px szerokosci
 	frame = vs.read()
 	frame = imutils.resize(frame, width=400)
+		# prepare the image to be classified by our deep learning network
+	image = cv2.resize(frame, (28, 28))
+	image = image.astype("float") / 255.0
+	image = img_to_array(image)
+	image = np.expand_dims(image, axis=0)
+	(Zelek, Czekoladka) = model.predict(image)[0]
+	label = "Zelek"
+	probability = Zelek
+	# Sprawdzamy czy zostala wykryta czekoladka czy zelek (porownujemy prawdopodobienstwo)
+	if Czekoladka > Zelek:
+		#update labelu
+		label = "Czekoladka"
+		probability = Czekoladka
+		
